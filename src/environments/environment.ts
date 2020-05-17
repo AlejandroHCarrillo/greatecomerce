4c43fda3074c36345ae1ac8614215d25d989715c
Alejandro <el_grande_ahc@hotmail.com>
2020-05-17 18:46:46 -0500

    install firestore and tools

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase:{
+    apiKey: "AIzaSyBK6Kh47y4OPO3xjzL8fnF4BPeWnjENyxA",
+    authDomain: "greatecomerce.firebaseapp.com",
+    databaseURL: "https://greatecomerce.firebaseio.com",
+    projectId: "greatecomerce",
+    storageBucket: "greatecomerce.appspot.com",
+    messagingSenderId: "170069636564",
+    appId: "1:170069636564:web:0bf42904c39974bd53d241",
+    measurementId: "G-681X2NVCH3"
+  }
 };
 
 /*
