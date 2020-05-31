98f9e75e96b479723c46fc456bbfb90bb3147eef
Alejandro <el_grande_ahc@hotmail.com>
2020-05-30 21:35:09 -0500

    Shopping routing Module

--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -1,17 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { SharedModule } from 'shared/shared.module';
+
 import { ShippingFormComponent } from 'shopping/components/checkout/shipping-form.component';
 import { ShoppingCartSummaryComponent } from 'shopping/components/checkout/shopping-cart-summary.component';
-
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { MyOrdersComponent } from './components/my-orders/my-orders.component';
 import { OrderSuccessComponent } from './components/order-success/order-success.component';
 import { ProductFilterComponent } from './components/products/product-filter.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { AppRoutingModule } from 'app/app-routing.module';
+import { ShoppingRoutingModule } from './shopping-routing.module';
 
 @NgModule({
   declarations: [
@@ -26,10 +25,8 @@ import { AppRoutingModule } from 'app/app-routing.module';
   ],
   imports: [
     CommonModule,
-    AppRoutingModule,
-
-    FormsModule,
-    SharedModule
+    SharedModule,
+    ShoppingRoutingModule
   ],
   exports: [
     ShoppingCartSummaryComponent,
